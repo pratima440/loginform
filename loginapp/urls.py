88cdd9be4968ce551065bed939e36ada03641374
pratima <pratimatiwari87728772@gmail.com>
2026-04-09 21:31:20 +0530

    fixed problem

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('', views.login,name='root'),
     path('login/',views.user_login,name='login'),
     path('home/',views.home,name='home'),
 
