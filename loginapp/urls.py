dbd71caee8d02eba4a2b7e793eaa304c9dd2337a
pratima <pratimatiwari87728772@gmail.com>
2026-04-09 21:19:37 +0530

    add a path

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
+    path('', views.login,name='root'),
     path('login/',views.user_login,name='login'),
     path('home/',views.home,name='home'),
 
